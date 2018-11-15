165c1ba02c3e7665c1cf2a07656dc186817273b8
Marcos Araújo <marcos.txt@gmail.com>
2018-11-15 19:20:26 -0200

    correções

--- src/main/java/br/com/casadocodigo/loja/daos/LivroDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/LivroDAO.java
@@ -28,7 +28,12 @@ public class LivroDAO {
 	
 	 public List<Livro> demaisLivros() {
         String jpql = "select l from Livro l order by l.id desc";
-        return manager.createQuery(jpql, Livro.class).setFirstResult(6).getResultList();
+        return manager.createQuery(jpql, Livro.class).setFirstResult(5).getResultList();
 	 }
+
+	public Livro buscarPorId(Integer id) {
+	    String jpql = "select l from Livro l join fetch l.autores where l.id = :id";
+	    return manager.createQuery(jpql, Livro.class).setParameter("id", id).getSingleResult();
+	}
 
 }
