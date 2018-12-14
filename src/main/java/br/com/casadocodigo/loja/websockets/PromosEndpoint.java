5688162c589a1093a348cfb415e13bbbc888d6c5
Marcos Araújo <marcos.araujo@globalhitss.com.br>
2018-12-14 16:55:59 -0200

    correções

--- src/main/java/br/com/casadocodigo/loja/websockets/PromosEndpoint.java
+++ src/main/java/br/com/casadocodigo/loja/websockets/PromosEndpoint.java
@@ -1,13 +1,34 @@
 package br.com.casadocodigo.loja.websockets;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.inject.Inject;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import br.com.casadocodigo.loja.models.Promo;
+
 @ServerEndpoint(value = "/canal/promos")
 public class PromosEndpoint {
+	
+	@Inject
+    private UsuariosSession usuarios;
+	
+	public void send(Promo promo) {
+	    List<Session> sessions = usuarios.getUsuarios();
+
+	    for (Session session : sessions) {
+	        if(session.isOpen()) {
+	            try {
+	                session.getBasicRemote().sendText(promo.toJson());
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	}
 
     @OnOpen
     public void onMessage(Session session) {
